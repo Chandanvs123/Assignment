e0a876f3dee5e58cc9d32622c758116cb9a519f5
Chandanvs123 <83530179+Chandanvs123@users.noreply.github.com>
2021-05-02 12:16:11 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def test(request):
+    name = request.get_json().get('a','b')
+    return "Inputs received"
+
+def sort_comb(a,b):
+    #Combine the arrays
+    list = a + b
+    #Sort the combined array
+    list.sort()
+    return list
+	
